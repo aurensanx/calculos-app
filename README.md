3d45b51cf03e315d3fd15cc90b49528aa4b11cbb
Alberto Aurensanz <aaurensanz@hiberus.com>
2020-01-16 12:23:44 +0100

    Iconos

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+#Debug on device
+
+- ionic cordova run android --device -l -c
+
+#Build
+
 - ionic cordova build android --prod --release
 
 - keytool -genkey -v -keystore calculos-app.keystore -alias calculos -keyalg RSA -keysize 2048 -validity 10000 (first time)
@@ -7,5 +13,8 @@
 - jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore calculos-app.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk calculos
 
 - rm calculo-mental.apk && zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk calculo-mental.apk
+
+
+
 
 
--- src/app/pages/home/components/bar/bar.component.ts
+++ src/app/pages/home/components/bar/bar.component.ts
@@ -21,23 +21,26 @@ export class BarComponent implements OnInit {
     }
 
     ngOnInit() {
-        const tlh = new TimelineMax();
-        tlh.to('#bgChange', this.settingsService.gameSettings.gameTime, {attr: {x: -400}, ease: Linear.easeNone});
-        tlh.to('#bgChange', this.settingsService.gameSettings.gameTime / 2, {
-            attr: {fill: '#FFFF00'},
-            ease: Linear.easeNone
-        }, 0);
-        tlh.to('#bgChange', this.settingsService.gameSettings.gameTime / 2, {
-            attr: {fill: '#FF0000'},
-            ease: Linear.easeNone
-        }, this.settingsService.gameSettings.gameTime / 2);
-
-        tlh.eventCallback('onComplete', () => this.onTimeCompleted(this.router));
-
+        let tlh = new TimelineMax();
         this.subscription = this.store.pipe(select('formula')).subscribe((next) => {
             if (!this.formula) {
                 this.formula = next;
-                tlh.restart();
+                tlh = new TimelineMax();
+                tlh.to('#bgChange', this.settingsService.gameSettings.gameTime, {
+                    attr: {x: -400},
+                    ease: Linear.easeNone
+                });
+                tlh.to('#bgChange', this.settingsService.gameSettings.gameTime / 2, {
+                    attr: {fill: '#FFFF00'},
+                    ease: Linear.easeNone
+                }, 0);
+                tlh.to('#bgChange', this.settingsService.gameSettings.gameTime / 2, {
+                    attr: {fill: '#FF0000'},
+                    ease: Linear.easeNone
+                }, this.settingsService.gameSettings.gameTime / 2);
+
+                tlh.eventCallback('onComplete', () => this.onTimeCompleted(this.router));
+                // tlh.restart();
             } else {
                 this.formula = next;
                 tlh.time(Math.max(tlh.time() - this.settingsService.gameSettings.extraTime, 0));
